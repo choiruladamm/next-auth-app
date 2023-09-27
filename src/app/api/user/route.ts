b0375bd1a3a5bad604d47cd83f3949809dd9644c
choiruladamm <choiruladamm@gmail.com>
2023-09-27 17:06:23 +0700

    feat: validate register use zod

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,21 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 import { hash } from "bcrypt";
+import * as z from "zod";
+
+const userSchema = z.object({
+  username: z.string().min(1, "Username is required").max(100),
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have than 8 characters"),
+});
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { username, email, password } = body;
+    const { username, email, password } = userSchema.parse(body);
 
     const existingUsername = await db.user.findUnique({
       where: { username: username },
